852ec1681c3309cd4a2cd3623ec74439570c7b10
zakaria thitah <zakaria.thitah@gmail.com>
2025-06-23 16:50:12 +0100

    Set base for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,9 +6,11 @@ import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/Amin_Impact/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+    
   },
 })
